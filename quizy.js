54ad510a1a171637f56478b26a228bc36803fa1c
Rei Tanoue <tanouser463@gmail.com>
2021-08-22 17:58:24 +0900

    シャッフル機能を追加（ただし未完成）

--- quizy.js
+++ quizy.js
@@ -1,44 +1,126 @@
 'use strict';
+//画像配列
+let images = [ 
+    "https://d1khcm40x1j0f.cloudfront.net/quiz/34d20397a2a506fe2c1ee636dc011a07.png",
+    "https://d1khcm40x1j0f.cloudfront.net/quiz/512b8146e7661821c45dbb8fefedf731.png",
+    "https://d1khcm40x1j0f.cloudfront.net/quiz/ad4f8badd896f1a9b527c530ebf8ac7f.png",
+    "https://d1khcm40x1j0f.cloudfront.net/quiz/ee645c9f43be1ab3992d121ee9e780fb.png",
+    "https://d1khcm40x1j0f.cloudfront.net/quiz/6a235aaa10f0bd3ca57871f76907797b.png",
+    "https://d1khcm40x1j0f.cloudfront.net/quiz/0b6789cf496fb75191edf1e3a6e05039.png",
+    "https://d1khcm40x1j0f.cloudfront.net/quiz/23e698eec548ff20a4f7969ca8823c53.png",
+    "https://d1khcm40x1j0f.cloudfront.net/quiz/50a753d151d35f8602d2c3e2790ea6e4.png",
+    "https://d1khcm40x1j0f.cloudfront.net/words/8cad76c39c43e2b651041c6d812ea26e.png",
+    "https://d1khcm40x1j0f.cloudfront.net/words/34508ddb0789ee73471b9f17977e7c9c.png",
+];
 
-let selection_id =1
+//選択肢配列
+let quizeSet = [
+    ["たかなわ","こうわ","たかわ"],
+    ["かめいど","かめど","かめと"],
+    ["こうじまち","おかとまち","かゆまち"],
+    ["ごせいもん","おなりもん","おかどもん"],
+    ["とどろき","たたりき","たたら"],
+    ["せきこうい","いじい","しゃくじい"],
+    ["ざっしょく","ざっしき","ぞうしき"],
+    ["みとちょう","おかちまち","ごしろちょう"],
+    ["ろっこつ","ししぼね","しこね"],
+    ["こぐれ","こばく","こしゃく"],
+].map(shuffle);
 
-let clicked_option =document.getElementById("ans-0-a") //変数でclicked_optionを宣言
-document.getElementById('0-' + selection_id + '-0').addEventListener('click',function() {
-    document.getElementById("0-1-0").classList.add("succeeded"); //文字を白に、背景を青にする
-    clicked_option.style.display ="block"; //解説ボックスを表示する
-    document.getElementById("0-0-1").style.pointerEvents="none"; //たかわをクリック無効にする
-    document.getElementById("0-0-2").style.pointerEvents="none"; //こうわをクリック無効にする
-});
-
-
-document.getElementById('0-0-1').addEventListener('click',function() {
-    document.getElementById("0-0-1").classList.add("failed"); //文字を白に、背景を赤にする
-    document.getElementById("0-1-0").classList.add("succeeded"); //正解のボックスの色を変更
-    document.getElementById("ans-0-b").style.display ="block"; //解説ボックスを表示する
-    document.getElementById("0-1-0").style.pointerEvents="none"; //たかなわをクリック無効にする
-    document.getElementById("0-0-2").style.pointerEvents="none"; //こうわをクリック無効にする
-});
-
-
-document.getElementById('0-0-2').addEventListener('click',function() {
-    document.getElementById("0-0-2").classList.add("failed"); //文字を白に、背景を赤にする
-    document.getElementById("0-1-0").classList.add("succeeded"); //正解のボックスの色を変更
-    document.getElementById("ans-0-b").style.display ="block"; //解説ボックスを表示する
-    document.getElementById("0-1-0").style.pointerEvents="none"; //たかなわをクリック無効にする
-    document.getElementById("0-0-1").style.pointerEvents="none"; //たかわをクリック無効にする
-});
+//正解の配列
+let true_answers = ["たかなわ","かめいど","こうじまち","おなりもん","とどろき","しゃくじい","ぞうしき","おかちまち","ししぼね","こぐれ"]
+    
+    // let false_answers_1st = ["たかわ","かめど","おかとまち","ごせいもん","たたら","せきこうい","ざっしょく","みとちょう","しこね","こしゃく"]
+    // let false_answers_2nd = ["こうわ","かめと","かゆまち","おかどもん","たたりき","いじい","ざっしき","ごしろちょう","ろっこつ","こばく"]
     
 
 
+//シャッフル
+function shuffle(arr) {
+    for (let k = arr.length - 1; k > 0; k--) { // i = ランダムに選ぶ終点のインデックス
+      const j = Math.floor(Math.random() * (k + 1));  // j = 範囲内から選ぶランダム変数
+      [arr[j], arr[k]] = [arr[k], arr[j]]; // 分割代入 i と j を入れ替える
+    }
+    return arr;
+}
+
+
+//本体
+for(let i = 0; i < 10; i++) {
+    
+
+    let question_box =
+        `<h1>`+ `${i+1}.この地名はなんて読む？` + `</h1>`+
+
+        `<div class="quiz-image-container">`+
+            `<img class="quiz-image" src="` + images [i] + `">`+
+        `</div>`+
+        
+        `<div class="box1">`+
+            `<ul id="choices">`+
+                `<li id='${i}-0'>`+quizeSet[i][0] +`</li>`+
+                `<li id='${i}-1'>`+quizeSet[i][1]+`</li>`+
+                `<li id='${i}-2'>`+quizeSet[i][2]+`</li>`+
+            `</ul>`+
+            `</div>`+    
+        `<div class="quiz-result" id="ans-${i}-a">`+
+            `<p class ="quiz-result-title quiz-result-title-succeeded" >正解！</p>`+
+            `<p class ="quiz-result-explanation">正解は「`+true_answers [i]+`」です！</p>`+
+        `</div>`+
+        `<div class="quiz-result" id="ans-${i}-b">`+
+            `<p class ="quiz-result-title quiz-result-title-failed" >不正解！</p>`+
+            `<p class ="quiz-result-explanation">正解は「`+true_answers[i]+`」です！</p>`+
+        `</div>`;
+
+
+    document.getElementById('quiz-box').insertAdjacentHTML('beforeend',question_box);
+
+    let clicked_option_true = document.getElementById( i + "-0")
+    let clicked_option_false_1st = document.getElementById( i + "-1")
+    let clicked_option_false_2nd = document.getElementById( i + "-2")
+    let true_explanation = document.getElementById("ans-" + i + "-a")
+    let false_explanation = document.getElementById("ans-" + i  + "-b")
+    
+
+    
+
+        //正解を選択した際の動き
+        clicked_option_true.addEventListener('click',function() {
+            clicked_option_true.classList.add("succeeded"); //文字を白に、背景を青にする
+            true_explanation.style.display ="block"; //解説ボックスを表示する
+            clicked_option_false_1st.style.pointerEvents="none"; //不正解１をクリック無効にする
+            clicked_option_false_2nd.style.pointerEvents="none"; //不正解２をクリック無効にする
+        });
+    
+
+   
+        
+    //不正解⑴を選択した際の動き  
+        clicked_option_false_1st.addEventListener('click',function() {
+            clicked_option_false_1st.classList.add("failed"); //文字を白に、背景を赤にする
+            clicked_option_true.classList.add("succeeded"); //正解のボックスの色を変更
+            false_explanation.style.display ="block"; //解説ボックスを表示する
+            clicked_option_true.style.pointerEvents="none"; //正解をクリック無効にする
+            clicked_option_false_2nd.style.pointerEvents="none"; //不正解２をクリック無効にする
+        });
+    
+
+ 
+    //不正解⑵を選択した際の動き
+        clicked_option_false_2nd.addEventListener('click',function() {
+            clicked_option_false_2nd.classList.add("failed"); //文字を白に、背景を赤にする
+            clicked_option_true.classList.add("succeeded"); //正解のボックスの色を変更
+            false_explanation.style.display ="block"; //解説ボックスを表示する
+            clicked_option_true.style.pointerEvents="none"; //正解をクリック無効にする
+            clicked_option_false_1st.style.pointerEvents="none"; //不正解１をクリック無効にする
+        });
+    
+
+};
 
 
 
 
 
 
-
-
-
-
-//let choices = document.getElementById('choices');
 
