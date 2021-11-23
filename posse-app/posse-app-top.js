f0ed9580d79ec34180a333c0dbd7148042803b37
Rei Tanoue <tanouser463@gmail.com>
2021-11-23 21:05:51 +0900

    cssの動きを整えた

--- posse-app/posse-app-top.js
+++ posse-app/posse-app-top.js
@@ -11,13 +11,12 @@
     type: 'bar',
     data: {
      //凡例のラベル
-      labels: ["","2","","4","","6","","8","","10","","","","","","","","","","","","","","","","","","","","","","",""],
+      labels: ["","2","","4","","6","","8","","10","","12","","14","","16","","18","","20","","22","","24","","26","","28","","30",""],
       datasets: [
         {
           label: '', //データ項目のラベル
           data: [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6], //グラフのデータ,
           backgroundColor: "rgb(32, 91, 255)"
-          
         }
       ]
     },
@@ -25,12 +24,14 @@
       legend:{
         display:false
       },
+
       scales: {
+
         xAxes: [{
           display: true,
           stacked: false,
           gridLines: {
-            display: false           
+            display: false
           }
         }],
         yAxes: [{
@@ -39,7 +40,7 @@
             display:false
           },
           ticks: {
-            // 目盛にドル記号を入れる
+            // 目盛に記号を入れる
             callback: function(value) {
                 return value + 'h';
             }
@@ -48,6 +49,41 @@
       },
     }
   });
+
+// Chart.types.Bar.extend({
+//   name: "graph-area",
+//   initialize: function (data) {
+//     Chart.types.Bar.prototype.initialize.apply(this, arguments);
+
+//     if (this.options.curvature !== undefined && this.options.curvature <= 1) {
+//       var rectangleDraw = this.datasets[0].bars[0].draw;
+//       var self = this;
+//       var radius = this.datasets[0].bars[0].width * this.options.curvature * 0.5;
+
+//       // override the rectangle draw with ours
+//       this.datasets.forEach(function (dataset) {
+//         dataset.bars.forEach(function (bar) {
+//           bar.draw = function () {
+//             // draw the original bar a little down (so that our curve brings it to its original position)
+//             var y = bar.y;
+//             // the min is required so animation does not start from below the axes
+//             bar.y = Math.min(bar.y + radius, self.scale.endPoint - 1);
+//             // adjust the bar radius depending on how much of a curve we can draw
+//             var barRadius = (bar.y - y);
+//             rectangleDraw.apply(bar, arguments);
+
+//             // draw a rounded rectangle on top
+//             Chart.helpers.drawRoundedRectangle(self.chart.ctx, bar.x - bar.width / 2, bar.y - barRadius + 1, bar.width, bar.height, barRadius);
+//             ctx.fill();
+
+//             // restore the y value
+//             bar.y = y;
+//           }
+//         })
+//       })
+//     }
+//   }
+// });
 
 
 
@@ -59,7 +95,7 @@
     data: {
       labels: ["JavaScript", "CSS", "PHP", "HTML","Laravel","SQL","SHELL","情報システム基礎知識（その他）"], //データ項目のラベル
       datasets: [{
-          borderWidth: 0, 
+          borderWidth: 0,
           backgroundColor: [
               "#0345EC",
               "#0F71BD",
@@ -68,15 +104,15 @@
               "#20BDDE",
               "#6D46EC",
               "#4A17EF",
-              "#3105C0",
-              
+              "#3105C0"
           ],
           data: [42, 18, 10, 10,7,6,4,3] //グラフのデータ
       }]
     },
     options: {
+
       legend:false,
-      responsive: true,
+      responsive: false,
       maintainAspectRatio: false,
       title: {
         display: true,
@@ -96,19 +132,18 @@
     data: {
       labels: ["ドットインストール","N予備校","POSSE課題"], //データ項目のラベル
       datasets: [{
-          borderWidth: 0, 
+          borderWidth: 0,
           backgroundColor: [
               "#0345EC",
               "#0F71BD",
               "#20BDDE"
-              
           ],
           data: [42,33,25] //グラフのデータ
       }]
     },
     options: {
       legend:false,
-      responsive: true,
+      responsive: false,
       maintainAspectRatio: false,
       title: {
         display: true,
@@ -120,89 +155,14 @@
 
 
 
+  // チェックボックスの動き
+for (let i = 0; i < 11; i++){
+  let clicked_checkbox = document.getElementById('clicked_checkbox_' + i);
+  let clicked_content = document.getElementById("content-checkbox-" + i);
+  clicked_checkbox.addEventListener('click', function () {
+    clicked_checkbox.classList.toggle("modal-checkbox-checked");
+    clicked_content.classList.toggle("content-checkbox-checked")
+    clicked_checkbox.classList.toggle("modal-checkbox__checkbox");
 
 
-
-
-
-
-
-
-  
-
-//   // カレンダー
-
-//   const week = ["日", "月", "火", "水", "木", "金", "土"];
-//   const today = new Date();
-//   // 月末だとずれる可能性があるため、1日固定で取得
-//   var showDate = new Date(today.getFullYear(), today.getMonth(), 1);
-  
-//   // 初期表示
-//   window.onload = function () {
-//       showProcess(today, calendar);
-//   };
-//   // 前の月表示
-//   function prev(){
-//       showDate.setMonth(showDate.getMonth() - 1);
-//       showProcess(showDate);
-//   }
-  
-//   // 次の月表示
-//   function next(){
-//       showDate.setMonth(showDate.getMonth() + 1);
-//       showProcess(showDate);
-//   }
-  
-//   // カレンダー表示
-//   function showProcess(date) {
-//       var year = date.getFullYear();
-//       var month = date.getMonth();
-//       document.querySelector('#header').innerHTML = year + "年 " + (month + 1) + "月";
-  
-//       var calendar = createProcess(year, month);
-//       document.querySelector('#calendar').innerHTML = calendar;
-//   }
-  
-// // カレンダー作成
-// function createProcess(year, month) {
-//   // 曜日
-//   var calendar = "<table><tr class='dayOfWeek'>";
-//   for (var i = 0; i < week.length; i++) {
-//       calendar += "<th>" + week[i] + "</th>";
-//   }
-//   calendar += "</tr>";
-
-//   var count = 0;
-//   var startDayOfWeek = new Date(year, month, 1).getDay();
-//   var endDate = new Date(year, month + 1, 0).getDate();
-//   var lastMonthEndDate = new Date(year, month, 0).getDate();
-//   var row = Math.ceil((startDayOfWeek + endDate) / week.length);
-
-//   // 1行ずつ設定
-//   for (var i = 0; i < row; i++) {
-//       calendar += "<tr>";
-//       // 1colum単位で設定
-//       for (var j = 0; j < week.length; j++) {
-//           if (i == 0 && j < startDayOfWeek) {
-//               // 1行目で1日まで先月の日付を設定
-//               calendar += "<td class='disabled'>" + (lastMonthEndDate - startDayOfWeek + j + 1) + "</td>";
-//           } else if (count >= endDate) {
-//               // 最終行で最終日以降、翌月の日付を設定
-//               count++;
-//               calendar += "<td class='disabled'>" + (count - endDate) + "</td>";
-//           } else {
-//               // 当月の日付を曜日に照らし合わせて設定
-//               count++;
-//               if(year == today.getFullYear()
-//                 && month == (today.getMonth())
-//                 && count == today.getDate()){
-//                   calendar += "<td class='today'>" + count + "</td>";
-//               } else {
-//                   calendar += "<td>" + count + "</td>";
-//               }
-//           }
-//       }
-//       calendar += "</tr>";
-//   }
-//   return calendar;
-// }
+})}
